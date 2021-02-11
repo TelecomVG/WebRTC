802764105b58d52c3b05ec3628cbab2482984b7a
Guillermo Lucio <guillermo.lucio@gmail.com>
2021-02-11 14:35:04 -0600

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "WebRTC", path: "WebRTC.xcframework")
+    ]
+)
